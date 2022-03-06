d58e2afb2c48307f05778fadb9d15074e1b61773
X123M3-256 <edward@knaphill.me.uk>
2022-03-06 22:21:58 +0000

    Add sprite packing algorithm

--- src/image.h
+++ src/image.h
@@ -19,6 +19,7 @@ void image_copy(image_t* src,image_t* dst);
 void image_blit(image_t* dst,image_t* src,int16_t x_offset,int16_t y_offset);
 int image_read_png(image_t* image,FILE* file);
 int image_write_png(image_t* image,FILE* file);
+void image_create_atlas(image_t* image,image_t* images,int num_images,int* x_offsets,int* y_offsets);
 void image_crop(image_t* image);
 void image_destroy(image_t* image);
 #endif // IMAGE_H_INCLUDED
